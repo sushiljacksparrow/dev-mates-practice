4d02663e7f422a3b446cc4ab77c438c8541d9449
Sushil Singh <sushils@amazon.com>
2019-09-10 08:59:13 -0400

    flood-fill

---
+++ flood-fill.py
@@ -0,0 +1,24 @@
+
+dir = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
+def flood_fill(image, temp, sr, sc, start_color, new_color):
+    temp[sr][sc] = 1
+    image[sr][sc] = new_color
+    for i in range(4):
+        if sr + dir[i][0] >=0 and sr + dir[i][0] < len(image) and sc + dir[i][1] >=0 and sc + dir[i][1] < len(image[1]):
+            if temp[sr + dir[i][0]][sc + dir[i][1]] == 0 and image[sr + dir[i][0]][sc + dir[i][1]] == start_color:
+                print(sr + dir[i][0], sc + dir[i][1])
+                flood_fill(image, temp, sr + dir[i][0], sc + dir[i][1], start_color, new_color)
+
+
+
+if __name__ == "__main__":
+    image = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+
+    sr = 1
+    sc = 1
+    new_color = 2
+    temp = [[0 for x in range(len(image[0]))] for y in range(len(image))]
+
+    flood_fill(image, temp, sr, sc, image[sr][sc], new_color)
+    print(image)
--- large-island.py
+++ large-island.py
@@ -3,7 +3,7 @@ dir = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 def island(grid, temp, i, j, t, l, m):
     temp[i][j] = 1
     count = t
-    for x in range(len(dir)):
+    for x in enumerate(dir):
             if i + dir[x][0] >=0 and i + dir[x][0] < l and j + dir[x][1] >= 0 and j + dir[x][1] < m:
                 if temp[i + dir[x][0]][j + dir[x][1]] == 0 and grid[i + dir[x][0]][j + dir[x][1]] == 1:
                     count = max(count, island(grid, temp, i + dir[x][0], j + dir[x][1], t + 1, l, m))
